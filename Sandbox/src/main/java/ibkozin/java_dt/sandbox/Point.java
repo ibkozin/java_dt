3de77efc35d7a3c69ae7c742fe9e695c29d5e7a2
ibkozin <ibkozin@gmail.com>
2018-06-15 02:42:57 -0500

    задание 2 - решение

--- Sandbox/src/main/java/ibkozin/java_dt/sandbox/Point.java
+++ Sandbox/src/main/java/ibkozin/java_dt/sandbox/Point.java
@@ -4,18 +4,27 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Point {
-    double p1;
-    double p2;
+    double x;
+    double y;
 
-    public Point(double p1, double p2) {
-        this.p1 = p1;
-        this.p2 = p2;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public static double distance (Point p1, Point p2){
+        double distance = Math.sqrt((p1.x-p2.x) * (p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
+        return distance;
     }
     public static void main(String[] args) throws Exception{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         double x1=Double.parseDouble(reader.readLine());
         double y1=Double.parseDouble(reader.readLine());
-        Point point=new Point(x1,y1);
+        double x2=Double.parseDouble(reader.readLine());
+        double y2=Double.parseDouble(reader.readLine());
+        Point p1 = new Point(x1,y1);
+        Point p2 = new Point(x2,y2);
+        System.out.println(distance(p1, p2));
+
 
     }
 
