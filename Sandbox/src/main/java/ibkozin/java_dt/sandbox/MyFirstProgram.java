efd6f8ac3b59040443f90b33fda4b0aa9b9ee96c
ibkozin <ibkozin@gmail.com>
2018-06-16 12:20:10 -0500

    добавлено несколько функций

--- Sandbox/src/main/java/ibkozin/java_dt/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ibkozin/java_dt/sandbox/MyFirstProgram.java
@@ -3,6 +3,24 @@ package ibkozin.java_dt.sandbox;
 public class MyFirstProgram {
 
 public static void main(String[] args) {
-System.out.println("Hello, world!");
+    hello("USA");
+    hello("RUSSIA");
+    hello("CANADA");
+    double l = 5;
+    System.out.println("площадь квадрата со стороной " + l + " = " +area(l));
+    double a = 3;
+    double b = 4;
+    System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " = " +area(a, b));
+
 }
+
+    public static void hello (String somebody){
+    System.out.println("Hello, " + somebody + "!");
+}
+    public static double area(double l){
+    return l*l;
+    }
+    public static double area(double a, double b){
+    return a*b;
+    }
 }
---
+++ Sandbox/src/test/java/ibkozin/java_dt/sandbox/SquareTests.java
@@ -0,0 +1,7 @@
+package ibkozin.java_dt.sandbox;
+
+public class SquareTests {
+    public void testArea(){
+        Square s = new Square(5);
+    }
+}
