5d4f28842737bc6f043049de3b3f9ed2bbdd93ec
ibkozin <ibkozin@gmail.com>
2018-06-16 23:50:00 -0500

    class Point modified

--- Sandbox/src/main/java/ibkozin/java_dt/sandbox/Point.java
+++ Sandbox/src/main/java/ibkozin/java_dt/sandbox/Point.java
@@ -11,19 +11,19 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-    public static double distance (Point p1, Point p2){
-        double distance = Math.sqrt((p1.x-p2.x) * (p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
+    public double distance (Point p){
+        double distance = Math.sqrt((this.x-p.x) * (this.x-p.x) + (this.y-p.y)*(this.y-p.y));
         return distance;
     }
     public static void main(String[] args) throws Exception{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        /* BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         double x1=Double.parseDouble(reader.readLine());
         double y1=Double.parseDouble(reader.readLine());
         double x2=Double.parseDouble(reader.readLine());
         double y2=Double.parseDouble(reader.readLine());
         Point p1 = new Point(x1,y1);
         Point p2 = new Point(x2,y2);
-        System.out.println(distance(p1, p2));
+        System.out.println(p1.distance(p2)); */
 
 
     }
