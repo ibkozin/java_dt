4f7d42500fe5c8a1b4e0ec81c0c7e7b24b884c02
ibkozin <ibkozin@gmail.com>
2018-06-24 17:11:50 -0500

    Добавлен crossBrowsers

--- Sandbox/src/test/java/ibkozin/java_dt/sandbox/PointTests.java
+++ Sandbox/src/test/java/ibkozin/java_dt/sandbox/PointTests.java
@@ -7,8 +7,8 @@ public class PointTests {
     @Test
     public void testDistance() {
         Point p1 = new Point(2, 4);
-        Point p2 = new Point(4, );
-        Assert.assertEquals(p1.distance(p2), 4.47213595499958);
+        Point p2 = new Point(4, 6);
+        Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
     }
 
 }
